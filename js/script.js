2be174499b9b8db26a4586afc9b8bcb71e716b47
hakancqn <hakancantonoglu19055@gmail.com>
2023-10-05 22:02:27 +0300

    Add: Lang

--- js/script.js
+++ js/script.js
@@ -1,43 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Tüm sayfaları gizle
-    document.querySelectorAll(".col > div").forEach(function (page) {
-        page.style.display = "none";
-    });
-
-    // Varsayılan olarak ilk bağlantıyı aktif yap
-    var defaultLink = document.querySelector("#menu a:first-child");
-    if (defaultLink) {
-        defaultLink.classList.add("active");
-        var defaultTarget = defaultLink.getAttribute("href");
-        document.querySelector(defaultTarget).style.display = "block";
-    }
-
-    // Menü bağlantılarına tıklama olayını dinle
-    document.querySelectorAll("#menu a").forEach(function (link) {
-        link.addEventListener("click", function (e) {
-            e.preventDefault();
-
-            // Tıklanan bağlantının hedefini al
-            var target = link.getAttribute("href");
-
-            // Tüm sayfaları gizle
-            document.querySelectorAll(".col > div").forEach(function (page) {
-                page.style.display = "none";
-            });
-
-            // Hedef sayfayı göster
-            document.querySelector(target).style.display = "block";
-
-            // Tüm menü bağlantılarından "active" sınıfını kaldır
-            document.querySelectorAll("#menu a").forEach(function (menuLink) {
-                menuLink.classList.remove("active");
-            });
-
-            // Tıklanan bağlantıya "active" sınıfını ekle
-            link.classList.add("active");
-        });
-    });
-
     var theme = document.body.getAttribute("data-bs-theme");
     var btn = document.getElementById('themeChanger');
     var logo = document.getElementById("logo");
@@ -52,6 +13,18 @@ document.addEventListener("DOMContentLoaded", function () {
         logo.style.webkitFilter = "invert(1)";
         logo.style.filter = "invert(1)";
     }
+
+    const langSelect = document.getElementById('lang');
+    const selectedValue = langSelect.value;
+    const divs = document.querySelectorAll('div[lang]');
+
+    divs.forEach(function(div) {
+        if (div.getAttribute('lang') === selectedValue) {
+            div.style.display = 'block';
+        } else {
+            div.style.display = 'none';
+        }
+    });
 });
 
 function themeChange(){
@@ -70,3 +43,18 @@ function themeChange(){
         logo.style.filter = "invert(1)";
     }
 }
+
+const langSelect = document.getElementById('lang');
+
+langSelect.addEventListener('change', function() {
+    const selectedValue = langSelect.value;
+    const divs = document.querySelectorAll('div[lang]');
+
+    divs.forEach(function(div) {
+        if (div.getAttribute('lang') === selectedValue) {
+            div.style.display = 'block';
+        } else {
+            div.style.display = 'none';
+        }
+    });
+});
