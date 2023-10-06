19df6da7a6a5366f88c04e2470a8ecaffd912cc4
hakancqn <hakancantonoglu19055@gmail.com>
2023-10-06 20:25:02 +0300

    Update

--- js/script.js
+++ js/script.js
@@ -31,16 +31,21 @@ function themeChange(){
     var theme = document.body.getAttribute("data-bs-theme");
     var btn = document.getElementById('themeChanger');
     var logo = document.getElementById("logo");
+    var logocommercial = document.getElementById("logo-commercial");
     if (theme === "dark" || !theme) {
         document.body.setAttribute("data-bs-theme", "light");
         btn.style.color = "darkblue"
         logo.style.webkitFilter = "";
         logo.style.filter = "";
+        logocommercial.style.webkitFilter = "";
+        logocommercial.style.filter = "";
     } else {
         document.body.setAttribute("data-bs-theme", "dark");
         btn.style.color = "lightblue"
         logo.style.webkitFilter = "invert(1)";
         logo.style.filter = "invert(1)";
+        logocommercial.style.webkitFilter = "invert(1)";
+        logocommercial.style.filter = "invert(1)";
     }
 }
 
