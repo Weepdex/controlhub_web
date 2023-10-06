000374872fd13569ed9596ed7126bbbec7dd1c18
hakancqn <hakancantonoglu19055@gmail.com>
2023-10-06 20:58:35 +0300

    Update

--- js/script.js
+++ js/script.js
@@ -2,16 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
     var theme = document.body.getAttribute("data-bs-theme");
     var btn = document.getElementById('themeChanger');
     var logo = document.getElementById("logo");
+    var logocommercial = document.getElementById("logo-commercial");
     if (theme === "dark" || !theme) {
-        document.body.setAttribute("data-bs-theme", "light");
-        btn.style.color = "darkblue"
-        logo.style.webkitFilter = "";
-        logo.style.filter = "";
-    } else {
-        document.body.setAttribute("data-bs-theme", "dark");
         btn.style.color = "lightblue"
         logo.style.webkitFilter = "invert(1)";
         logo.style.filter = "invert(1)";
+        logocommercial.style.webkitFilter = "invert(1)";
+        logocommercial.style.filter = "invert(1)";
+    } else {
+        btn.style.color = "darkblue"
+        logo.style.webkitFilter = "";
+        logo.style.filter = "";
     }
 
     const langSelect = document.getElementById('lang');
@@ -37,8 +38,6 @@ function themeChange(){
         btn.style.color = "darkblue"
         logo.style.webkitFilter = "";
         logo.style.filter = "";
-        logocommercial.style.webkitFilter = "";
-        logocommercial.style.filter = "";
     } else {
         document.body.setAttribute("data-bs-theme", "dark");
         btn.style.color = "lightblue"
