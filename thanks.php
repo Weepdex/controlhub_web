c4727eb86d97da5e8e553f02960038b255c9a6a3
Hasan Bulut <buluthasan853@gmail.com>
2023-11-16 16:38:50 +0300

    update

--- thanks.php
+++ thanks.php
@@ -1,13 +1,10 @@
 <?php
-
-$file_url = 'https://sicanzi.com/images/nah.png';
+$file_url = 'http://localhost/img/logo.png';
 header('Content-Type: application/octet-stream');
 header("Content-Transfer-Encoding: Binary");
-header("Content-disposition: attachment; filename=\"" . "weepdex.png" . "\"");
+header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\"");
 readfile($file_url);
-
 require_once __DIR__. '/counter/counter.php';
-
 addUniqueHit();
 header("Location: /thanks.html");
 die();
