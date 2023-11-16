e44d60755344187aca3dd05d8f807ea3b674fc56
Hasan Bulut <buluthasan853@gmail.com>
2023-11-16 16:41:51 +0300

    fixed

--- thanks.php
+++ thanks.php
@@ -6,5 +6,5 @@ header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\
 readfile($file_url);
 require_once __DIR__. '/counter/counter.php';
 addUniqueHit();
-header("Location: /thanks.html");
+header("Location: /thanks");
 die();
